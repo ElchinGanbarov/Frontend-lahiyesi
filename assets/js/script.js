40dda6994372b5895509d1887dabca886f9fc45b
ElcinGanbarov <elchinig@code.edu.az>
2019-12-09 03:49:49 +0400

    section-caruselslide,mostpopular,aboutus,ourcourses

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,75 @@
-$(document).ready(function(){
-    $(".close").click(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($("html").scrollTop() > 200) {
+            $("#edu-global").addClass("fixed-top");
+            $("#edu-global").css("background-color","white")
+        } else {
+            $("#edu-global").removeClass("fixed-top");
+        }
+    }); 
+    $(".close").click(function () {
         $("#alertbox").addClass("d-none")
-        $("#header").css("padding-top","0")
+        $("#header").css("padding-top", "0")
 
     })
-    $(".nav-item").hover(function(){
+    $(".nav-item").hover(function () {
         $(this).find(".dropdown-menu").addClass("show");
         $(this).find(".galer").removeClass("show");
         $(this).find(".search-menu").removeClass("show");
-        
 
-    },function(){
+
+    }, function () {
         $(this).find(".dropdown-menu").removeClass("show");
     })
-    $(".title ").hover(function(){
-    $(".galer").addClass("show");
+    $(".title ").hover(function () {
+        $(".galer").addClass("show");
 
-    },function(){
+    }, function () {
         $(".galer").removeClass("show")
     })
-    $(".icon").click(function(){
+    $(".icon").click(function () {
         $(this).toggleClass("fa-times");
-        if($(this).hasClass("fa-times")){
+        if ($(this).hasClass("fa-times")) {
             $(this).parent().next().addClass("d-block")
-        }else{
+        } else {
             $(this).parent().next().removeClass("d-block")
-        } 
+        }
     })
-      $("#carusel-slide").hover(function(){
-        $(this).find(".carousel-control-prev-icon").addClass("d-block");
-        $(this).find(".carousel-control-next-icon").addClass("d-block")
-        
-      },function(){
-        $(this).find(".carousel-control-prev-icon").removeClass("d-block");
-        $(this).find(".carousel-control-next-icon").removeClass("d-block");
-      })
+    if($("#carusel-slide").length){
+        $("#carusel-slide").hover(function () {
+            $(this).find(".carousel-control-prev-icon").addClass("d-block");
+            $(this).find(".carousel-control-next-icon").addClass("d-block")
+    
+        }, function () {
+            $(this).find(".carousel-control-prev-icon").removeClass("d-block");
+            $(this).find(".carousel-control-next-icon").removeClass("d-block");
+        })
+    }
+   
+    if($(".owl-carousel").length){
+        $('.owl-carousel').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,       
+    })
+    }
+        if($(".counter")){
+            $(".counter").counterUp({
+                time: 1500,
+                
+            });
+        }
+       
+        if($(".courses-item").length){
+           $(".courses-item").hover(function(){
+            $(this).find(".courses-img img").css({
+               "transform":"scale(1.1)"
+                    })
+           },function(){
+            $(this).find(".courses-img img").css({
+                "transform":"scale(1.0)"
+                     })
+           })
+        }
 })
